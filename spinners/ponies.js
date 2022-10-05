5657915322ad6b914bcb42a4036ce8ad69df87b0
bentacobuck <thor@risingempire.tech>
2022-10-05 15:42:13 -0400

    wrote some stuff lol

--- spinners/ponies.js
+++ spinners/ponies.js
@@ -1,7 +1,25 @@
 document.getElementById("spinner")
+document.getElementById("spinPoints")
+document.getElementById("up")
+var spinPoints2 = 0;
+var spc = 1;
+var cost = 100;
 
-function spinPoints (
-    var msg = `$ ${}`
-)
+function addSpinPoints () {
+    spinPoints2 += spc;
+    var msg = `$ ${spinPoints2}`;
+    spinPoints.innerHTML = msg;
+}
 
-spinner.addEventListener(mouseover , )
+function upgrade () {
+    if (spinPoints2 > cost) {
+        spinPoints2 -= cost;
+        spc += 1;
+        cost += 100;
+        var msg2 = `upgrade spinner $ ${cost}`;
+        up.innerHTML = msg2;
+    }
+}
+
+ spinner.addEventListener("mouseover", addSpinPoints)
+up.addEventListener("click", upgrade)
