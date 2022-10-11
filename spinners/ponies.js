c948565f146f7f77c9359c840c584ebedfee0911
bentacobuck <thor@risingempire.tech>
2022-10-11 09:29:40 -0400

    Made a Spring Green and Navajowhite Spinners for Spinner Sim!

--- spinners/ponies.js
+++ spinners/ponies.js
@@ -5,6 +5,7 @@ var up = document.querySelector("#up");
 var spinPoints2 = 0;
 var spc = 1;
 var cost = 100;
+var level = 1;
 
 function addSpinPoints () {
     spinPoints2 += spc;
@@ -19,8 +20,24 @@ function upgrade () {
         cost += 100;
         var msg2 = `upgrade spinner $ ${cost}`;
         up.innerHTML = msg2;
-        spinner.classList.replace("spinner","spinner2");
+        level += 1;
     }
+
+    if (level === 2) {
+        spinner.classList.replace("spinner","spinner2");
+        spc = spc * 2
+    }
+
+    if (level === 5) {
+        spinner.classList.replace("spinner2","spinner3");
+        spc = spc * 3
+    }
+
+    if (level === 10) {
+        spinner.classList.replace("spinner3","spinner4");
+        spc = spc * 5
+    }
+
 }
 
  spinner.addEventListener("mouseover", addSpinPoints)
